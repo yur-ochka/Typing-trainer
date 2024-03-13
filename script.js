7a9b72fedb592c8e987de97adc8d4af1838d7b76
Yurchenko Illia <iliayurchenko06@gmail.com>
2024-03-13 19:28:28 +0200

    User can only print when focused on textarea and text will only appear when is typed correctly

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
+let charNumber = 0;
+const mainKeys = ["Tab", "Backspace", "CapsLock", "Enter", "ShiftLeft", "ShiftRight"];
 document.addEventListener('keydown', function(event) {
-    var keyCode = event.code;
-    console.log('Key pressed:', keyCode);
-    const key = document.querySelector(`.${keyCode}`);
-    key.style.background = `#1e2125`;
+    if (document.querySelector("#typingArea") == document.activeElement)
+    {
+        var keyCode = event.code;
+        console.log('Key pressed:', keyCode);
+        const key = document.querySelector(`.${keyCode}`);
+        key.style.background = `#1e2125`;
+        const textHintArea = document.querySelector(`#hintArea`);
+        if (event.key === 'Backspace') {
+            event.preventDefault();
+        }
+        if(textHintArea.value[charNumber] == event.key)
+        {
+            charNumber++;
+        }
+        else {
+            if (mainKeys.includes(keyCode) != true){
+                key.style.background = "red";
+            }
+            event.preventDefault();
+        }
+    }
+    
 });
 
 document.addEventListener('keyup', function(event) {
