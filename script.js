4891d228b3e4fd0bf5fdc7618b53a6f49779f8df
Yurchenko Illia <iliayurchenko06@gmail.com>
2024-03-14 18:20:11 +0200

    Added info panel, containing information about user`s typing

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 let charNumber = 0;
+
+let firstCharPrinted = false;
+let countMistakes = 0;
+let expiredTime = 0;
+let remainingTime = 120;
+let countPrintedChars = 0;
+let typingSpeed = 0;
+
+let mistakes = document.querySelector(".mistakes");
+let time = document.querySelector(".time");
+let speed = document.querySelector(".speed");
+let charsCount = document.querySelector(".charsCount");
+
+let interval = null;
+
 const mainKeys = ["Tab", "Backspace", "CapsLock", "Enter", "ShiftLeft", "ShiftRight"];
+
 document.addEventListener('keydown', function(event) {
     if (document.querySelector("#typingArea") == document.activeElement)
     {
+        if (firstCharPrinted == false) {
+            firstCharPrinted = true;
+            interval = setInterval(updateInfo, 1000);      
+        }
         var keyCode = event.code;
         console.log('Key pressed:', keyCode);
         const key = document.querySelector(`.${keyCode}`);
@@ -14,11 +34,15 @@ document.addEventListener('keydown', function(event) {
         if(textHintArea.value[charNumber] == event.key)
         {
             charNumber++;
+            countPrintedChars++;
+            charsCount.innerHTML = `Printed chars: ${countPrintedChars}`;
         }
         else {
             if (mainKeys.includes(keyCode) != true){
                 key.style.background = "red";
-            }
+                countMistakes++;
+                mistakes.innerHTML = `Mistakes: ${countMistakes}`;
+            }        
             event.preventDefault();
         }
     }
@@ -31,3 +55,26 @@ document.addEventListener('keyup', function(event) {
     const key = document.querySelector(`.${keyCode}`);
     key.style.background = `#383f47`;
 });
+
+let updateInfo = () =>
+{
+    remainingTime--;
+    expiredTime++;
+    time.innerHTML = `Remaining time: ${remainingTime}s`;
+    if (remainingTime == 0) 
+    {
+        clearInterval(interval);
+        document.querySelector(`#typingArea`).disabled = true;
+        countSpeed();
+    }
+}
+
+let countSpeed = () =>
+{
+    typingSpeed = roundToTwoDecimalPlaces(countPrintedChars / expiredTime);
+    speed.innerHTML = `Chars per minute: ${typingSpeed}`;
+}
+
+function roundToTwoDecimalPlaces(number) {
+    return Math.round(number * 100) / 100;
+}
